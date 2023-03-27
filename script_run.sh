93873037a015949e1c96bcc3c1d70c48aff832d7
Xingyun SA <saxonsa5683@163.com>
2023-03-27 12:20:42 +0100

    add code Summarization on GraphCodeBERT

---
+++ script_run.sh
@@ -0,0 +1,14 @@
+lang=ruby #programming language
+lr=5e-5
+batch_size=16
+beam_size=10
+source_length=256
+target_length=128
+data_dir=dataset
+output_dir=model/$lang
+train_file=$data_dir/$lang/train.jsonl
+dev_file=$data_dir/$lang/valid.jsonl
+epochs=20 
+pretrained_model=microsoft/graphcodebert-base #Roberta: roberta-base
+
+python run.py --lang $lang --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
